f0d9e6ef77cac103ac6d154c61f19623dec8dcc0
Andrew Webb <webba@ihopu.org>
2021-09-10 15:29:23 -0500

    created landing page with practice plan detail modal

---
+++ src/components/session/SessionList.js
@@ -0,0 +1,120 @@
+import React, { useContext, useEffect, useState } from "react"
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { SessionContext } from "./SessionProvider.js"
+import { useHistory, Link } from "react-router-dom"
+import "./Session.css"
+
+export const SessionList = () => {
+    const { sessions, getSessions } = useContext(SessionContext)
+    const history = useHistory()
+    const today = new Date()
+    today.setDate(today.getDate() - 1)
+    const completedSessions = sessions.filter(session => Date.parse(session.date) < today)
+    const scheduledSessions = sessions.filter(session => Date.parse(session.date) >= today)
+
+    
+    
+
+    useEffect(() => {
+        getSessions()       
+    }, [])
+
+    return (
+        <>
+        <h1 className="header">Practice Plan</h1>
+        <div className="practicePlan-buttons">
+            <button className="practicePlan-button" onClick={() => history.push("/session/create")}>Schedule Session?</button>
+            <button className="practicePlan-button" onClick={() => history.push("/practiceplan/create")}>Create Practice Plan?</button>
+            <button className="practicePlan-button" onClick={() => history.push("/exercise/create")}>Create Practice Exercise?</button>
+        </div>
+        <div className="sessions">
+            <h2>Scheduled Sessions</h2>
+        {scheduledSessions.map(session => {
+            const PracticePlanDetail = (props) => {
+                const {
+                buttonLabel,
+                className
+                } = props;
+            
+                const [modal, setModal] = useState(false);
+            
+                const toggle = () => setModal(!modal);
+  
+                return (
+                <div>
+                    <div color="danger" onClick={toggle}>{buttonLabel} <u><strong>{session.practice_plan.title}</strong></u></div>
+                    <Modal isOpen={modal} toggle={toggle} className={className}>
+                    <ModalHeader toggle={toggle}></ModalHeader>
+                    <ModalBody>
+                    <h3>{session.practice_plan.title}</h3>
+                        <div>Player: {session.player.user.username}</div>
+                        <div>Plan Description: {session.practice_plan.description}</div>
+                        <h4>Exercises</h4>
+                        {session.practice_plan.exercises.map(exercise => {
+                            return (
+                                <>
+                                    <div>{exercise.title}</div>
+                                    <div>{exercise.description}</div>
+                                </>
+                            )
+                        })}
+                    </ModalBody>
+                    </Modal>
+                </div>
+                );
+            }
+            return (
+            <>
+            <PracticePlanDetail />
+            <div>{session.date} {session.length_of_session} minutes</div>
+            <div>{session.notes}</div>
+            </>
+        )})}
+        </div>
+        <div className="sessions">
+            <h2>Completed Sessions</h2>
+            {completedSessions.map(session => {
+                const PracticePlanDetail = (props) => {
+                    const {
+                    buttonLabel,
+                    className
+                    } = props;
+                
+                    const [modal, setModal] = useState(false);
+                
+                    const toggle = () => setModal(!modal);
+      
+                    return (
+                    <div>
+                        <div color="danger" onClick={toggle}>{buttonLabel} <u><strong>{session.practice_plan.title}</strong></u></div>
+                        <Modal isOpen={modal} toggle={toggle} className={className}>
+                        <ModalHeader toggle={toggle}></ModalHeader>
+                        <ModalBody>
+                        <h3>{session.practice_plan.title}</h3>
+                            <div>Player: {session.player.user.username}</div>
+                            <div>Plan Description: {session.practice_plan.description}</div>
+                            <h4>Exercises</h4>
+                            {session.practice_plan.exercises.map(exercise => {
+                                return (
+                                    <>
+                                        <div>{exercise.title}</div>
+                                        <div>{exercise.description}</div>
+                                    </>
+                                )
+                            })}
+                        </ModalBody>
+                        </Modal>
+                    </div>
+                    );
+                }
+            return (
+            <>
+            <PracticePlanDetail />
+            <div>{session.date} {session.length_of_session} minutes</div>
+            <div>{session.notes}</div>
+            </>
+        )})}
+        </div>
+        </>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter as Router } from "react-router-dom"
 import { PracticePlan } from "./components/PracticePlan.js"
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
